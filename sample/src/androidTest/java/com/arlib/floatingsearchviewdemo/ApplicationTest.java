c69270ac123ad14cac2cb68461042bb867d03049
Hellyna NG <me@hellyna.com>
2020-09-18 02:28:59 +0800

    Fix tests

--- sample/src/androidTest/java/com/arlib/floatingsearchviewdemo/ApplicationTest.java
+++ sample/src/androidTest/java/com/arlib/floatingsearchviewdemo/ApplicationTest.java
@@ -1,13 +1,17 @@
 package com.arlib.floatingsearchviewdemo;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.content.Context;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+    @Test
+    public void useAppContext() {
+        Context c = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Assert.assertEquals(c.getPackageName(), "com.arlib.floatingsearchviewdemo");
     }
 }
